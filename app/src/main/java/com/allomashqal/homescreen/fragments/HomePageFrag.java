bf606d3908d42c9db13b4cb74f4627c849008f7f
Itsmanprchahal <chahalmanpreet0305@gmail.com>
2021-01-27 22:25:58 +0530

    update

--- app/src/main/java/com/allomashqal/homescreen/fragments/HomePageFrag.java
+++ app/src/main/java/com/allomashqal/homescreen/fragments/HomePageFrag.java
@@ -1,7 +1,6 @@
 package com.allomashqal.homescreen.fragments;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.res.Resources;
@@ -10,17 +9,16 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.allomashqal.R;
@@ -29,20 +27,14 @@ import com.allomashqal.Utils.Utility;
 import com.allomashqal.controller.Controller;
 import com.allomashqal.helper.LocaleHelper;
 import com.allomashqal.homescreen.adapters.HomePageAdapter;
-import com.allomashqal.homescreen.adapters.PaginationListener;
 import com.allomashqal.homescreen.response.SalonListResponse;
 import com.allomashqal.sharedpref.BaseFragment;
 
-import org.checkerframework.checker.units.qual.A;
-
 import java.util.ArrayList;
-import java.util.Collection;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import retrofit2.Response;
-
-import static com.allomashqal.homescreen.adapters.PaginationListener.PAGE_START;
 
 
 public class HomePageFrag extends BaseFragment implements Controller.SalonListAPI {
@@ -51,7 +43,8 @@ public class HomePageFrag extends BaseFragment implements Controller.SalonListAP
     @BindView(R.id.recyler_view)
     RecyclerView recyler_view;
     HomePageAdapter homePageAdapter;
-
+    NestedScrollView nestedScrollView;
+    ProgressBar progress_bar;
     View view;
     String locale;
     int itemCount = 0;
@@ -59,7 +52,8 @@ public class HomePageFrag extends BaseFragment implements Controller.SalonListAP
     Utility utility;
     Controller controller;
     Resources resources;
-    ArrayList<SalonListResponse.Data.List> lists;
+    ArrayList<SalonListResponse.Data.List> lists = new ArrayList<>();
+    int page = 1, limit;
 
     private static int currentPage;
     private static int NUM_PAGES;
@@ -71,6 +65,8 @@ public class HomePageFrag extends BaseFragment implements Controller.SalonListAP
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_home_page, container, false);
         ButterKnife.bind(this, view);
+        nestedScrollView = view.findViewById(R.id.nestedScrollView);
+        progress_bar = view.findViewById(R.id.progress_bar);
         utility = new Utility();
         pd = new ProgressDialog(getContext());
         pd.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -87,8 +83,28 @@ public class HomePageFrag extends BaseFragment implements Controller.SalonListAP
         resources = context.getResources();
         // type = getStringVal(Constants.TYPE);
 
-        controller.SalonList("salon", getStringVal(Constants.LAT), getStringVal(Constants.LNG), "1");
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        manager.setOrientation(LinearLayout.VERTICAL);
+        recyler_view.setLayoutManager(manager);
+        homePageAdapter = new HomePageAdapter(getContext(), locale, resources, lists);
+        recyler_view.setAdapter(homePageAdapter);
+        getData(page, limit);
 
+        nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                if (scrollY == v.getChildAt(0).getMeasuredHeight() - v.getMeasuredHeight()) {
+                    page++;
+                    if (page<=limit)
+                    {
+                       progress_bar.setVisibility(View.VISIBLE);
+                        getData(page, limit);
+                    }else  {
+                        progress_bar.setVisibility(View.GONE);
+                    }
+                }
+            }
+        });
 
         return view;
     }
@@ -98,23 +114,26 @@ public class HomePageFrag extends BaseFragment implements Controller.SalonListAP
     @Override
     public void onSuccessSalonList(Response<SalonListResponse> success) {
         pd.dismiss();
-        lists = new ArrayList<>();
-        if (success.isSuccessful())
-        {
+        //lists = new ArrayList<>();
+        if (success.isSuccessful()) {
+            limit = success.body().getData().getTotalPage();
             for (int i = 0; i < success.body().getData().getList().size(); i++) {
                 SalonListResponse.Data.List list = success.body().getData().getList().get(i);
                 lists.add(list);
-                LinearLayoutManager manager = new LinearLayoutManager(getContext());
-                manager.setOrientation(LinearLayout.VERTICAL);
-                recyler_view.setLayoutManager(manager);
+
                 homePageAdapter = new HomePageAdapter(getContext(), locale, resources, lists);
                 recyler_view.setAdapter(homePageAdapter);
             }
-        }else  {
+        } else {
             Toast.makeText(getContext(),""+success.message(),Toast.LENGTH_LONG).show();
         }
 
 
+    }
+
+    private void getData(int pagee, int limit) {
+progress_bar.setVisibility(View.VISIBLE);
+        controller.SalonList("salon", getStringVal(Constants.LAT), getStringVal(Constants.LNG), String.valueOf(pagee));
     }
 
     @Override
